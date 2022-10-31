69deb84ffabfdbd1ac3fef9c07da597fdd7b5690
Engiineeer <ntpzlwo@gmail.com>
2022-10-31 19:13:38 +0300

    done

--- flights/views.py
+++ flights/views.py
@@ -2,11 +2,12 @@ from .models import Booking, Flight
 from rest_framework.generics import ListAPIView
 from flights.models import Flight
 from .seializers import FlightsListSerializer, UpcomingBookingSerializer
+from django.utils import timezone
 
 class FlightsListView(ListAPIView):
     queryset = Flight.objects.all()
     serializer_class = FlightsListSerializer
 
 class UpcomingBookingView(ListAPIView):
-    queryset = Booking.objects.all()
+    queryset = Booking.objects.filter(date__gt=timezone)
     serializer_class = UpcomingBookingSerializer
